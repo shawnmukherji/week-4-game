f6bc0750e2ab84fc1bf0c2f66a15e595c1b9b3c9
Shawn Mukherji <shawnmukherji@gmail.com>
2017-05-28 09:30:48 -0500

    changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(function()) {
+
+	crystals = ['assets/images/crystal-1.png','assets/images/crystal-2.png','assets/images/crystal-3.png','assets/images/crystal-4.png'];
+
+	var counter = 0;
+	var wins = 0;
+	var losses = 0;
+
+	$('#wins').text(wins);
+	$('#losses').text(losses);
+
+	newCrystals();
+	newGame();
+
+
+	function newCrystals(){
+
+		var numbers = []
+			while(numbers.)
+	}
+}
